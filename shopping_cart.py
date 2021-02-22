3a5a56f6a8075eca437a6041b7672aca5ae26dd0
aredfern15 <77812013+aredfern15@users.noreply.github.com>
2021-02-21 21:29:19 -0500

    Assemble running total price

--- shopping_cart.py
+++ shopping_cart.py
@@ -27,6 +27,8 @@ products = [
 # INFO CAPTURE / INPUT
 #
 
+total_price = 0 
+
 while True:
     pass
     selected_id = input("Please input a product identifier: ") #> "9" (string)
@@ -36,6 +38,7 @@ while True:
     else: 
         matching_products =  [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0] 
+        total_price = total_price + matching_product["price"]
         print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 
@@ -43,6 +46,9 @@ while True:
 #
 # INFO DISPLAY / OUTPUT
 #
+
+
+print("TOTAL PRICE: " + str(total_price))
 
 #A grocery store name of your choice 
 # A grocery store phone number and/or website URL and/or address of choice 
